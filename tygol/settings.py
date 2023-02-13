b74043600363be529d0133a61d1c28a550ea59a7
hung222 <90371118+hung222@users.noreply.github.com>
2023-02-13 20:08:15 +0700

    Update settings.py

--- tygol/settings.py
+++ tygol/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
-
-ALLOWED_HOSTS = ["django22.onrender.com"]
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
